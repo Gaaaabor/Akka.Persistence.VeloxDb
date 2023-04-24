09a41bc5d0f1d7d31fd356df997c5a376cb2b732
Bocsó Gábor <bocsgab@gmail.com>
2023-04-24 23:21:05 +0200

    LocalVeloxDbFixture path fix

--- Akka.Persistence.VeloxDb.Test/LocalVeloxDbFixture.cs
+++ Akka.Persistence.VeloxDb.Test/LocalVeloxDbFixture.cs
@@ -8,8 +8,6 @@ namespace Akka.Persistence.VeloxDb.Test
     {
         private readonly Process _process;
 
-        private static bool _started;
-
         public int ReplayMaxMessageCount { get; set; }
         public string Address { get; set; }
 
@@ -18,24 +16,25 @@ namespace Akka.Persistence.VeloxDb.Test
             Address = "localhost:7568";
             ReplayMaxMessageCount = 1000;
 
-            //string dbPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Akka.Persistence.VeloxDb.Db");
-            //_process = new Process
-            //{
-            //    StartInfo =
-            //    {
-            //        FileName = "dotnet",
-            //        Arguments = "run",
-            //        UseShellExecute = true,
-            //        WorkingDirectory = dbPath
-            //    }
-            //};
+            string dbPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName, "Akka.Persistence.VeloxDb.Db");
+            _process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "dotnet",
+                    Arguments = "run",
+                    UseShellExecute = false,
+                    WorkingDirectory = dbPath
+                }
+            };
 
-            //_process.Start();
+            _process.Start();
         }
 
         public void Dispose()
         {
-            //_process?.Dispose();
+            _process?.Kill();
+            _process?.Dispose();
         }
     }
 }
